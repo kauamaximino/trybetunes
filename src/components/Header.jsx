d2ebdacce9b5a25a3d5ad6e2d4681647e5568cb5
Kauã Maximino <kaua_maximino@outlook.com>
2022-03-10 23:08:27 -0300

    requisito 4 ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
@@ -23,11 +24,18 @@ class Header extends React.Component {
   render() {
     const { loading, user } = this.state;
     return (
-      <header data-testid="header-component">
-        {loading
-          ? <p>Carregando...</p>
-          : <h1 data-testid="header-user-name">{ user }</h1>}
-      </header>
+      <>
+        <header data-testid="header-component">
+          {loading ? <p>Carregando...</p>
+            : <h1 data-testid="header-user-name">{user}</h1>}
+        </header>
+
+        <nav>
+          <Link data-testid="link-to-search" to="/search">Search</Link>
+          <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+          <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+        </nav>
+      </>
     );
   }
 }
