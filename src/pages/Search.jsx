b7b738ced7e931d9f7b2dbb4520d29231f8cb490
Kauã Maximino <kaua_maximino@outlook.com>
2022-03-11 14:49:18 -0300

    requisito 5 em andamento

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,31 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputArtist: '',
+    };
+  }
+
+  btnSearchDisabled = () => {
+    const { inputArtist } = this.state;
+    console.log(inputArtist);
+  }
+
   render() {
     return (
       <div data-testid="page-search">
         <Header />
+
+        <form>
+          <input data-testid="search-artist-input" type="text" />
+
+          <button data-testid="search-artist-button" type="button">
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
